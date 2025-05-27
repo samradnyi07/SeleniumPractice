ab164c9d8051235eafc031f8e7e42dbc18e9888b
SamradnyiDeshpande <samradnyideshpande0711@gmail.com>
2025-05-27 23:26:22 +0530

    Dyanamic dropdown added

---
+++ AllProjects/src/test/java/Selenium/AllProjects/DynamicDropdown.java
@@ -0,0 +1,28 @@
+package Selenium.AllProjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		
+		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_originStation1_CTNR']//a[@value='BLR']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='MAA']")).click();
+		
+		/*
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click(); */
+		
+	}
+
+}
