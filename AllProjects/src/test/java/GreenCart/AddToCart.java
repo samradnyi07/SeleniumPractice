35f2157e2f66289e84866a84da4598c5ad257b4f
SamradnyiDeshpande <samradnyideshpande0711@gmail.com>
2025-06-04 17:08:53 +0530

    add to cart code added

--- AllProjects/src/test/java/GreenCart/AddToCart.java
+++ AllProjects/src/test/java/GreenCart/AddToCart.java
@@ -1,5 +1,6 @@
 package GreenCart;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddToCart {
 
@@ -15,8 +19,26 @@ public class AddToCart {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
-		Thread.sleep(3000);
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		WebDriverWait w = new WebDriverWait(driver,Duration.ofSeconds(10));
+	//	Thread.sleep(3000);
 		String[] items = {"Cucumber","Brocolli","Tomato","Cashews"};
+		add(driver,items);
+		Thread.sleep(1000);
+		//check cart
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.cssSelector("div[class='cart-preview active'] div[class='action-block'] button")).click();
+		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class ='promoCode']")));
+		driver.findElement(By.xpath("//input[@class ='promoCode']")).sendKeys("rahulshettyacademy");
+		w.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("button.promoBtn"))));
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='promoInfo']")));
+		System.out.println(driver.findElement(By.cssSelector("span[class='promoInfo']")).getText());
+		
+		driver.close();
+	}
+	public static void add(WebDriver driver,String[] items) {
 		List<WebElement> names =driver.findElements(By.xpath("//h4[@class='product-name']"));
 		List<String> ls = Arrays.asList(items);
 		int j=0;
@@ -36,14 +58,6 @@ public class AddToCart {
 			}
 			
 		}
-		Thread.sleep(1000);
-		//check cart
-		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
-		driver.findElement(By.cssSelector("div[class='cart-preview active'] div[class='action-block'] button")).click();
-		
-	
-		
-		
 	}
 
 }
